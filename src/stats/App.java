dc0c7eb6fe4b279fbc330327cea57d6f5758ebb5
fabianaritorti <fabianaritorti@host-131-114-208-195.m.unipi.it>
2020-01-21 14:43:33 +0100

    Change of App Class

--- src/stats/App.java
+++ src/stats/App.java
@@ -1,23 +1,11 @@
 package stats;
 	
-import java.net.SocketTimeoutException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.mongodb.MongoSocketOpenException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import stats.model.Player;
-import stats.persistence.DAOException;
-import stats.persistence.DAOPlayerMongo;
-import stats.persistence.ReadFromFile;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 
 public class App extends Application {
@@ -27,16 +15,10 @@ public class App extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("/stats/view/Main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/stats/view/application.css").toExternalForm());
+			primaryStage.setMinHeight(739);
+			primaryStage.setMinWidth(866);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			String json = new ReadFromFile().readLocalJSON("/Users/kenobi_gen/Serie A 2019-20- Players.json");
-			Gson gson = new Gson();
-			Player[] players = gson.fromJson(json, Player[].class);
-			System.out.println("Number: " + players.length);
-			List<Player> playerList = Arrays.asList(players);
-			new DAOPlayerMongo().createListOfPlayers(playerList);
-		} catch(DAOException msoe) {
-			System.out.println("Non sei collegato alla VPN");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
